a06932d4168fd0925807bb08f339168b37ae14fc
aivaras-vilkas <vilkas.aivaras@gmail.com>
2026-06-15 20:35:22 +0300

    Add Axe accesibility test

---
+++ tests/accessibility/homepage-accessibility.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test.describe('Homepage accesibility tests @accessibility', () => {
+
+  test('Homepage should have no critical accessibility violations', async ({ page }) => {
+    await page.goto('/');
+
+    const results = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa'])
+      .analyze();
+
+    console.log('Accessibility Violations:', results.violations);
+    const serious = results.violations.filter(v => 
+      v.impact === 'serious' || v.impact === 'critical'
+    );
+    expect(serious).toEqual([]);
+  });
+});
